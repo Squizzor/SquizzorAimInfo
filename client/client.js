9864ed0ab60c4cd01f5790e0359a7d942d9092eb
Mario Kramer <mario-kramer@live.de>
2021-03-20 15:13:54 +0100

    * improved output format

--- client/client.js
+++ client/client.js
@@ -6,18 +6,18 @@ const AIMPOS_KEY = 77; // M
 alt.on("keyup", function (key) {
     if (key == AIMPOS_KEY) {
         if (alt.Player.local.isAiming) {
-            console.log('[Aiming info:]');
+            console.log('[Aiming info]');
 
             var entityFound = natives.getEntityPlayerIsFreeAimingAt(alt.Player.local);
             if (entityFound[0]) {
                 var entityHash = natives.getEntityModel(entityFound[1]);
-                console.log('Entity hash: 0x' + entityHash.toString(16).toUpperCase());
+                console.log('  Entity hash: 0x' + entityHash.toString(16).toUpperCase());
                 
                 var entityPosition = natives.getEntityCoords(entityFound[1], false);
-                console.log('Entity position: (X: ' + entityPosition.x + ' Y: ' + entityPosition.y + ' Z: ' + entityPosition.z + ')');
+                console.log('  Entity position: (X: ' + entityPosition.x + ' Y: ' + entityPosition.y + ' Z: ' + entityPosition.z + ')');
             }
 
-            console.log('Aiming position: (X: ' + alt.Player.local.aimPos.x + ' Y: ' + alt.Player.local.aimPos.y + ' Z: ' + alt.Player.local.aimPos.z + ')');
+            console.log('  Aiming position: (X: ' + alt.Player.local.aimPos.x + ' Y: ' + alt.Player.local.aimPos.y + ' Z: ' + alt.Player.local.aimPos.z + ')');
         }
     }
 });
